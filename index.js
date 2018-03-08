e623e65dc2f8c29aa702c08acd25a3a559efa114
Anthony Chang <awchang12@gmail.com>
2018-03-08 20:12:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ var specialGuest = "Neil deGrasse Tyson"
 var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
-https://learn.co/tracks/technical-application-javascript/introduction/welcome/strings-lab#
+
 var conversation = `${topic}`;
